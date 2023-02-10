c9c445839cd2d3a0f74308f425d9c72295271b89
yooonwodyd <yoonwodyd00@naver.com>
2023-02-10 21:01:52 +0900

    수정

---
+++ src/main/java/com/haedal/User.java
@@ -0,0 +1,24 @@
+package com.haedal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long userId;
+
+    private String name;
+
+    private String phone;
+
+}
