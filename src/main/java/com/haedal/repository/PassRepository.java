c5464f0069968bb31b02d491637b6f58cfc6950c
oyoungsun <plusmate@naver.com>
2023-02-11 20:52:54 +0900

    #15 추가 설정 update

--- src/main/java/com/haedal/repository/PassRepository.java
+++ src/main/java/com/haedal/repository/PassRepository.java
@@ -1,6 +1,7 @@
 package com.haedal.repository;
 
 import com.haedal.entity.Booking;
+import com.haedal.entity.Pass;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PassRepository  extends JpaRepository<Pass, Long> {
