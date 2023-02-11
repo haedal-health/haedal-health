b4a541ce0c2b4eefb517e1aedd382ace67d113aa
oyoungsun <plusmate@naver.com>
2023-02-11 20:22:26 +0900

    #15 createTest

--- src/main/java/com/haedal/repository/PassRepository.java
+++ src/main/java/com/haedal/repository/PassRepository.java
@@ -3,6 +3,6 @@ package com.haedal.repository;
 import com.haedal.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PassRepository  extends JpaRepository<Booking, Long> {
+public interface PassRepository  extends JpaRepository<Pass, Long> {
 
 }
--- src/test/java/com/haedal/entity/PassTest.java
+++ src/test/java/com/haedal/entity/PassTest.java
@@ -1,8 +1,31 @@
 package com.haedal.entity;
 
+import com.haedal.repository.PassRepository;
+import com.haedal.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassTest {
 
+    @Autowired
+    private PassRepository passRepository;
+
+    @Test
+    public void createPassEntityThanReturnsavedPassEntity(){
+        Pass pass = new Pass();
+        pass.setPassId(1L);
+        pass.setName("해달헬스장 1일 이용권");
+        pass.setPrice(9000);
+        pass.setCount(1);
+        pass.setStartedDay(LocalDateTime.now().minusDays(1));
+        pass.setEndedDay(LocalDateTime.now());
+
+        Pass savedPass = passRepository.save(pass);
+
+    }
 
 }
