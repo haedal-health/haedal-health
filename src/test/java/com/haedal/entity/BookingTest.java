427d5323e0e81ffcdb82121d8de6d4271291efc2
oyoungsun <plusmate@naver.com>
2023-02-14 21:18:13 +0900

    #27 - 공유

--- src/test/java/com/haedal/entity/BookingTest.java
+++ src/test/java/com/haedal/entity/BookingTest.java
@@ -2,6 +2,8 @@ package com.haedal.entity;
 
 import com.haedal.entity.Booking;
 import com.haedal.repository.BookingRepository;
+import com.haedal.repository.PassRepository;
+import com.haedal.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,88 +20,76 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@DisplayName("UserEntity 및 Repository 테스트")
+@DisplayName("BookingEntity 매핑관계 테스트")
 public class BookingTest {
     @Autowired
     private BookingRepository bookingRepository;
 
-    @Test
-    @DisplayName("booking 생성 테스트")
-    public void createBookingEntityThanReturnsavedBookingEntity(){
-        Booking booking = new Booking();
+    @Autowired
+    private UserRepository userRepository;
 
-        booking.setStartTime(LocalDateTime.now().plusHours(1));
-        booking.setEndedTime(LocalDateTime.now().plusHours(3));
-
-        Booking savedBooking = bookingRepository.save(booking);
-
-    }
-    @Test
-    @DisplayName("Booking id로 조회 테스트")
-    public void detailTest_findByName_returnPassEntity(){
-        Booking booking = new Booking();
-
-        //booking.setBookingId(1L);
-        booking.setStartTime(LocalDateTime.now().plusHours(1));
-        booking.setEndedTime(LocalDateTime.now().plusHours(3));
-
-        Booking savedBooking = bookingRepository.save(booking);
-
-
-        Booking findedBooking = bookingRepository.findById(savedBooking.getBookingId()).orElse(null);
-
-        assertEquals(findedBooking.getBookingId(), savedBooking.getBookingId());
-        assertEquals(findedBooking.getStartTime(), savedBooking.getStartTime());
-        assertEquals(findedBooking.getEndedTime(), savedBooking.getEndedTime());
-    }
+    @Autowired
+    private PassRepository passRepository;
 
     @Test
-    @DisplayName("booking 수정 테스트")
-    public void update(){
-        Booking booking = new Booking();
+    @DisplayName("매핑 생성 테스트")
+    void createBookingWithMapping_test(){
+        Pass pass = new Pass();
+        pass.setName("해달헬스장 1일 이용권");
+        pass.setPrice(9000);
+        pass.setCount(1);
+        pass.setStartedDay(LocalDateTime.now().minusDays(1));
+        pass.setEndedDay(LocalDateTime.now());
+        Pass savedPass = passRepository.save(pass);
 
+        User user = new User();
+        user.setName("홍길동");
+        user.setPhone("01012345678");
+        User savedUser = userRepository.save(user);
+
+        Booking booking = new Booking();
         booking.setStartTime(LocalDateTime.now().plusHours(1));
         booking.setEndedTime(LocalDateTime.now().plusHours(3));
-
+        booking.setUserId(savedUser.getUserId());
+        booking.setPassId(savedPass.getPassId());
         Booking savedBooking = bookingRepository.save(booking);
-
-
-        Optional<Booking> findBooking = bookingRepository.findById(savedBooking.getBookingId());
-
-
-        findBooking.ifPresent(booking1 -> {
-            booking1.setStartTime(LocalDateTime.now().plusHours(4));
-            booking1.setEndedTime(LocalDateTime.now().plusHours(10));
-            Booking updatedBooking  = bookingRepository.save(booking1);
-
-
-            assertEquals(updatedBooking.getBookingId(), booking1.getBookingId());
-            assertEquals(updatedBooking.getStartTime(), booking1.getStartTime());
-            assertEquals(updatedBooking.getEndedTime(), booking1.getEndedTime());
-        });
 
     }
 
     @Test
-    @DisplayName("booking 삭제 테스트")
-    public void delete(){
-        Booking booking = new Booking();
+    @DisplayName("user 한 명이 가진 모든 이용권 조회하기(userId) - 여러개")
+    void findAllPassfromOneUser_test2(){
+        User user = new User();
+        user.setName("홍길동");
+        user.setPhone("01012345678");
+        User savedUser = userRepository.save(user);
 
-        booking.setStartTime(LocalDateTime.now().plusHours(1));
-        booking.setEndedTime(LocalDateTime.now().plusHours(3));
+        for(int i=0; i<3; i++) {
+            Pass pass = new Pass();
+            pass.setName("해달헬스장 1일 이용권" + (i+1) + "번째");
+            pass.setPrice(9000);
+            pass.setCount(1);
+            pass.setStartedDay(LocalDateTime.now().minusDays(1));
+            pass.setEndedDay(LocalDateTime.now());
+            Pass savedPass = passRepository.save(pass);
 
-        Booking savedBooking = bookingRepository.save(booking);
+            Booking booking = new Booking();
+            booking.setStartTime(LocalDateTime.now().plusHours(1));
+            booking.setEndedTime(LocalDateTime.now().plusHours(3));
+            booking.setUserId(savedUser.getUserId());
+            booking.setPassId(savedPass.getPassId());
+            bookingRepository.save(booking);
 
+        }
+        List<Booking> bookings = bookingRepository.findAllByUserId(savedUser.getUserId()).orElse(null);
 
-        Optional<Booking> findBooking = bookingRepository.findById(savedBooking.getBookingId());
-
-        findBooking.ifPresent(booking1 -> {
-            bookingRepository.delete(booking1);
-        });
-
-        Optional deleteBooking = bookingRepository.findById(1L);
-        assertFalse(deleteBooking.isPresent());
-
-
+        for(Booking b : bookings){
+            Pass pass1 = passRepository.findById(b.getPassId()).orElse(null);
+            if(pass1 != null){
+                System.out.println(pass1.getName());
+            }
+        }
+        assertEquals(bookings.size(), 3);
     }
+
 }
