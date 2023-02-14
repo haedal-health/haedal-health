ee11883727f6a67eceda0442a0472d946ebb559c
oyoungsun <plusmate@naver.com>
2023-02-14 19:47:02 +0900

    #31 - 불필요 어노테이션 제거

--- src/main/java/com/haedal/entity/Pass.java
+++ src/main/java/com/haedal/entity/Pass.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Pass {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
