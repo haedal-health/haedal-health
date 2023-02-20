011d2b3bcd39859b5ff03d86346fb15489e92187
yooonwodyd <yoonwodyd00@naver.com>
2023-02-20 18:46:38 +0900

    유저 조회 주소 /all 로 변경, createForm 주석처리

--- src/main/java/com/haedal/UserController.java
+++ src/main/java/com/haedal/UserController.java
@@ -1,8 +1,9 @@
-package com.haedal.repository;
+package com.haedal;
 
 
 import com.haedal.UserForm;
 import com.haedal.entity.User;
+import com.haedal.repository.UserRepository;
 import com.haedal.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,11 +18,14 @@ import java.util.List;
 public class UserController {
 
     private UserService userService ;
-
+    @Autowired
+    private UserRepository userRepository;
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+
 
     //사용자 생성페이지 요청
     @GetMapping("")
@@ -30,9 +34,10 @@ public class UserController {
         User user = new User();
         user.setName("윤재용");
         user.setPhone("01099105948");
-        return user;
+
          */
-        return "1";
+
+        return "유저 폼으로";
     }
 
     //사용자 생성 요청
@@ -41,19 +46,18 @@ public class UserController {
         User user = new User();
         user.setName(form.getName());
         user.setPhone(form.getPhone());
-
-        userService.sign(user);
+        userRepository.save(user);
         return "처음페이지로";
     }
 
 
 
     //사용자 전체 조회 페이지 요청
-    @GetMapping("")
-    public String list(Model model){
+    @GetMapping("all")
+    public List list(Model model){
         List<User> users = userService.findUsers();
-        model.addAttribute("users",users);
-        return "리스트 페이지로";
+        ;
+        return users;
     }
 
 
