35dd0a346882086038a588f8d42d7e6ec2b79b73
yooonwodyd <yoonwodyd00@naver.com>
2023-02-24 17:09:04 +0900

    #58 @ WebMvcTest 사용X -> 추후 변경 예정, 유저컨트롤러 테스트 작성 및 json 데이터 변환 번들 추가

--- src/test/java/com/haedal/controller/UserControllerTest.java
+++ src/test/java/com/haedal/controller/UserControllerTest.java
@@ -2,33 +2,75 @@
 package com.haedal.controller;
 
 
+import com.google.gson.Gson;
+import com.haedal.entity.User;
+import com.haedal.entity.UserDto;
+import com.haedal.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
 
-    @Autowired
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserService userService;
+
     private MockMvc mockMvc;
 
-    @DisplayName("유저 생성")
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
+
+
+    @DisplayName("회원 가입 성공")
     @Test // 생성
-    public void userCreateTest(){
+
+    public void userCreateTest() throws Exception {
+        //UserDto를 받으면 UserDto를 반환한다.
+        //userService.sign(user)의 경우에는 유저를 반환한다.
+        //given
+        UserDto userDto = newUserDto(1L); //post요청으로 받아올 값
+
+        Mockito.when(userService.sign(any(User.class))).thenReturn(UserDto.toEntity(userDto));
+
+        //when
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new Gson().toJson(userDto))
+                //then
+        ).andExpect(jsonPath("userId").value(1L))
+                .andExpect(jsonPath("name").value("홍길동"))
+                .andExpect(jsonPath("phone").value("01012345678"));
 
     }
 
@@ -36,32 +78,101 @@ public class UserControllerTest {
     @Test //
     public void userListTest() throws Exception{
 
-        String json = "[{\"userId\":1,\"name\":\"윤재용\",\"phone\":\"01099105948\"}]";
+        //given
+        UserDto userDto = newUserDto(1L);
+        String json = "[{\"userId\":1,\"name\":\"홍길동\",\"phone\":\"01012345678\"}]";
+        Mockito.when(userService.findUsers()).thenReturn(newUsers());
 
+
+        //when
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/user")
-                        .content(json)
                         .contentType(MediaType.APPLICATION_JSON))
-                //.andExpect(MockMvcResultMatchers.content().string(json))
                 .andExpect(status().isOk())
                 .andDo(MockMvcResultHandlers.print());
+
+
     }
 
     @DisplayName("유저 단일 id 조회")
     @Test
-    public void userFindTest(){
+    public void userFindTest () throws Exception {
+
+        //given
+        Mockito.when(userService.findOne(1L)).thenReturn(newUser(1L));
+
+        //when
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/user/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ) //then
+                .andExpect(jsonPath("userId").value(1L))
+                .andExpect(jsonPath("name").value("홍길동"))
+                .andExpect(jsonPath("phone").value("01012345678"));
 
     }
     @DisplayName("유저 단일 id 수정")
     @Test
-    public void userModifyTest(){
+    public void userModifyTest() throws Exception {
+
+        //given
+        Mockito.when(userService.modifyOne(eq(1L),any(User.class))).thenReturn(newUser(2L));
+
+        //when
+        mockMvc.perform(
+                        MockMvcRequestBuilders.patch("/user/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                ) //then
+                .andExpect(jsonPath("userId").value(2L))
+                .andExpect(jsonPath("name").value("홍길동"))
+                .andExpect(jsonPath("phone").value("01012345678"));
 
     }
 
     @DisplayName("유저 단일 id 삭제")
     @Test
-    public void userDeleteTest(){
+    public void userDeleteTest() throws Exception {
+        Mockito.when(userService.deleteOne(1L)).thenReturn("홍길동이 삭제되었습니다.");
+
+        //when
+        mockMvc.perform(
+                        MockMvcRequestBuilders.delete("/user/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                );//then
+                // .andExpect(content().string("홍길동이 삭제되었습니다."));
+                // 한글 지원 안됨으로 확인을 위해선 @WebMvcTest + addFilters(new CharacterEncodingFilter("UTF-8", true))  를 써야함
+
 
     }
+
+
+    private UserDto newUserDto(Long id){
+
+        UserDto userDto = UserDto.builder()
+                .userId(id)
+                .name("홍길동")
+                .phone("01012345678")
+                .build();
+
+        return userDto;
+    }
+
+    private User newUser(Long id){
+
+        User user = new User();
+        user.setUserId(id);
+        user.setName("홍길동");
+        user.setPhone("01012345678");
+        return user;
+    }
+
+    private List<User> newUsers(){
+        List<User> users = new ArrayList<>();
+        for(int i = 0; i< 10; i++)
+        {
+            users.add(newUser(i+1L));
+        }
+        return users;
+     }
 
 }
