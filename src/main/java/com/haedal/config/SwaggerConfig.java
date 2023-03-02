4f2dcf80a9e5eb882fdad138710a5c5cd8bdef19
yooonwodyd <yoonwodyd00@naver.com>
2023-03-02 14:36:43 +0900

    #67 swagger config 클래스 추가

--- src/main/java/com/haedal/config/SwaggerConfig.java
+++ src/main/java/com/haedal/config/SwaggerConfig.java
@@ -1,2 +1,18 @@
-package com.haedal.config;public class SwaggerConfig {
+package com.haedal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30);
+    }
 }
