480b2632aa182597f63aeb39ffb2491a8ff771c9
yooonwodyd <dbswodyd00@naver.com>
2023-05-11 10:53:53 +0900

    #88 카카오 로그인 링크로 이동, 로그인 후 화면, 유저 정보 가져오는 컨트롤러 작성

---
+++ src/main/java/com/haedal/entity/OAuthAttributes.java
@@ -0,0 +1,34 @@
+package com.haedal.entity;
+
+import java.util.Map;
+
+public enum OAuthAttributes {
+    
+    KAKAO("kakao", (attribute) -> {
+
+        Map<String, Object> account = (Map)attribute.get("kakao_account");
+        Map<String, String> profile = (Map)account.get("profile");
+
+        UserDto userDto = new UserDto.of(1,"1","1");
+        userProfile.setUserName(profile.get("nickname"));
+        userProfile.setEmail((String)account.get("email"));
+
+        return userProfile;
+    });
+
+    private final String registrationId; // 로그인한 서비스(ex) google, naver..)
+    private final Function<Map<String, Object>, UserProfile> of; // 로그인한 사용자의 정보를 통하여 UserProfile을 가져옴
+
+    OAuthAttributes(String registrationId, Function<Map<String, Object>, UserProfile> of) {
+        this.registrationId = registrationId;
+        this.of = of;
+    }
+
+    public static UserProfile extract(String registrationId, Map<String, Object> attributes) {
+        return Arrays.stream(values())
+                .filter(value -> registrationId.equals(value.registrationId))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new)
+                .of.apply(attributes);
+    }
+}
