43f6a33ccadf2023fd89f49bd2f7cfb2cf0db31c
yooonwodyd <dbswodyd00@naver.com>
2023-07-18 16:39:05 +0900

    #101 토큰에서 email 관련 정보만 파싱해오기

--- src/main/java/com/haedal/service/TokenParsingService.java
+++ src/main/java/com/haedal/service/TokenParsingService.java
@@ -1,2 +1,22 @@
-package com.haedal.service;public class TokenParsingService {
+package com.haedal.service;
+
+import com.haedal.model.entity.Pass;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class TokenParsingService {
+    public String getEmail(Authentication authentication){
+
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        Map<String, String> att = (Map<String, String>) attributes.get("kakao_account");
+        System.out.println(att.get("email"));
+        return att.get("email");
+    }
 }
