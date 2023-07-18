7dff4653f12698607be7a6d8397477822a8ddfc4
yooonwodyd <dbswodyd00@naver.com>
2023-07-18 16:37:54 +0900

    #101 admin이 아닌 유저가 pass 서비스에 접근시 예외 발생시키기

--- src/main/java/com/haedal/controller/PassController.java
+++ src/main/java/com/haedal/controller/PassController.java
@@ -8,6 +8,7 @@ import com.haedal.model.entity.User;
 import com.haedal.service.PassService;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.websocket.AuthenticationException;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,28 +22,28 @@ public class PassController {
 
     //Todo : @ResquestBody PassRequest passRequest
     @PostMapping("")
-    public Pass createPass(@RequestBody PassCreateRequest request) throws AuthenticationException {
+    public Pass createPass(@RequestBody PassCreateRequest request, Authentication authentication) throws AuthenticationException {
         PassDto pass = PassDto.of(request.getName(), request.getCount(), request.getPrice(), request.getStartedDay(), request.getEndedDay());
-        Pass savedPass = passService.create(pass);
+        Pass savedPass = passService.create(pass,authentication);
         //PassDto result = PassDto.from(savedPass);
         //Todo : return PassRespose result
         return savedPass;
     }
     @GetMapping("/{passId}")
-    public PassDto detailPass(@PathVariable Long passId){
-        PassDto pass = PassDto.from(passService.getPass(passId));
+    public PassDto detailPass(@PathVariable Long passId, Authentication authentication)throws AuthenticationException{
+        PassDto pass = PassDto.from(passService.getPass(passId,authentication));
         return pass;
     }
     @GetMapping("")
-    public List<PassDto> listPass(){
-        return passService.getAll();
+    public List<PassDto> listPass(Authentication authentication) throws AuthenticationException {
+        return passService.getAll(authentication);
     }
     @PatchMapping("/{passId}")
-    public PassDto modifyPass(@PathVariable Long passId, @RequestBody PassDto passDto) throws Exception {
-        return PassDto.from(passService.updatePass(passId, passDto));
+    public PassDto modifyPass(@PathVariable Long passId, @RequestBody PassDto passDto,Authentication authentication) throws Exception {
+        return PassDto.from(passService.updatePass(passId, passDto,authentication));
     }
     @DeleteMapping("/{passId}")
-    public String deletePass(@PathVariable Long passId) throws Exception {
-        return passService.deletePass(passId);
+    public String deletePass(@PathVariable Long passId,Authentication authentication) throws Exception {
+        return passService.deletePass(passId,authentication);
     }
 }
