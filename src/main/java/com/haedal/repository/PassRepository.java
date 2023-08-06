5b39a649dd95533c75ae977d99a8b4b45aa44cfe
yooonwodyd <dbswodyd00@naver.com>
2023-08-06 16:22:08 +0900

    #109 yaml 설정 변경

--- src/main/java/com/haedal/repository/PassRepository.java
+++ src/main/java/com/haedal/repository/PassRepository.java
@@ -1,6 +1,8 @@
 package com.haedal.repository;
 
+import com.haedal.model.PassDto;
 import com.haedal.model.entity.Pass;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
