d7928e4e045bb860635c1782e5303fdfa8746702
yooonwodyd <dbswodyd00@naver.com>
2023-08-17 20:18:44 +0900

    #113 jpa 추가 + booking sql 추가

--- src/main/java/com/haedal/repository/PassRepository.java
+++ src/main/java/com/haedal/repository/PassRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PassRepository {
+public interface PassRepository extends JpaRepository<Pass, Long> {
 
     public Optional<Pass> findByName(String name);
 
